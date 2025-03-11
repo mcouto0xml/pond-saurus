c186ea24a6f391cfcc30a1bdbb163d96219390ac
mcouto0xml <murilo.oliveira@sou.inteli.edu.br>
2025-03-11 15:18:59 -0300

    init-gh-pages

---
+++ ponderada/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/inteli/Área de trabalho/pond-saurus/ponderada/node_modules/infima/dist/css/default/default.css"),
+  require("/home/inteli/Área de trabalho/pond-saurus/ponderada/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/inteli/Área de trabalho/pond-saurus/ponderada/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/inteli/Área de trabalho/pond-saurus/ponderada/src/css/custom.css"),
+];
---
+++ ponderada/build/assets/js/36994c47.53ebc199.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkponderada=self.webpackChunkponderada||[]).push([[858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
